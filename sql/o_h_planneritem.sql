87383125416ca30f94354b51a2b7d506e2b6c00d
pafik13 <re1ax_88@mail.ru>
2014-01-08 21:53:53 +0400

    My files
    My files

---
+++ sql/o_h_planneritem.sql
@@ -0,0 +1,16 @@
+﻿USE one;
+CREATE TABLE one.o_h_planneritem (
+  IMEI int(11) NOT NULL,
+  HOSPITAL_ID int(11) NOT NULL,
+  WEEKNUM int(2) NOT NULL,
+  DAY_OF_WEEK varchar(9) NOT NULL,
+  FILE_ID int(11) NOT NULL,
+  INDEX IDX_o_h_planneritem (HOSPITAL_ID, IMEI),
+  UNIQUE INDEX UK_o_h_planneritem (HOSPITAL_ID, IMEI, DAY_OF_WEEK),
+  CONSTRAINT FK_o_h_planneritem_o_upload_files_ID FOREIGN KEY (FILE_ID)
+  REFERENCES one.o_upload_files (ID) ON DELETE RESTRICT ON UPDATE RESTRICT
+)
+ENGINE = INNODB
+CHARACTER SET utf8
+COLLATE utf8_general_ci
+COMMENT = 'Значения в планнировщике';
---
+++ sql/o_upload_files.sql
@@ -0,0 +1,17 @@
+﻿USE one;
+CREATE TABLE one.o_upload_files (
+  ID int(11) NOT NULL AUTO_INCREMENT,
+  IMEI int(11) NOT NULL,
+  TYPE varchar(255) NOT NULL,
+  HASH varchar(32) NOT NULL,
+  TIMESTAMP timestamp DEFAULT CURRENT_TIMESTAMP,
+  FILE mediumblob NOT NULL,
+  ERR text DEFAULT NULL,
+  PRIMARY KEY (ID)
+)
+ENGINE = INNODB
+AUTO_INCREMENT = 42
+AVG_ROW_LENGTH = 1589248
+CHARACTER SET utf8
+COLLATE utf8_general_ci
+COMMENT = 'Загруженные файлы на сервер';
